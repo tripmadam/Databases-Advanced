ee6f1bac81e7669a02567bc18492f985a0864a3d
tripmadam <stef749@hotmail.com>
2021-03-06 20:49:40 +0100

    added redis to mongodb functionality

--- webscraper.py
+++ webscraper.py
@@ -17,21 +17,7 @@ def scraper():
     soup = BeautifulSoup(URL.content, 'lxml')
         
     transactions = soup.find_all('div', attrs={'class': 'sc-6nt7oh-0 PtIAf'})
-    hashes = []
-    times = []
-    BTC = []
-    Dollars = []
-
-          
-    for x in range(transactions.__len__()):
-        if (x%4 == 0):
-            hashes.append(transactions[x].text)
-        if(x%4 == 1): 
-            times.append(transactions[x].text)
-        if(x%4 == 2):
-            BTC.append(float(transactions[x].text.replace(' BTC','')))
-        if(x%4 == 3):
-            Dollars.append(float(transactions[x].text.replace('$','').replace(',','')))
+   
         
     for x in range(transactions.__len__()):
         if(x%4 ==0):
@@ -39,43 +25,63 @@ def scraper():
             r.rpush(transactions[x].text ,transactions[x+1].text)
             r.rpush(transactions[x].text, float(transactions[x+2].text.replace(' BTC','')))
             r.rpush(transactions[x].text, float(transactions[x+3].text.replace('$','').replace(',','')))
-            r.expire(transactions[x].text, 60)
-            print(r.lrange(transactions[x].text,0,-1))
+            #r.expire(transactions[x].text, 60)
+            keylist.append(transactions[x].text )
+            #print(r.lrange(transactions[x].text,0,-1))
         
-            
+        
 
-    data = {'Hashes' : hashes, 'Time': times, 'Amount_BTC': BTC, 'Amount_Dollars': Dollars}
+def push():
+
+    valuelist = []
+
+    for x in keylist:
+       value = r.lrange(x,2,2)
+       valuelist.append(value)
+
+    maximumindex = valuelist.index(max(valuelist))
+
+    hashhighest = keylist[maximumindex]
+
+    topush = r.lrange(hashhighest,0,-1)
 
     
-    df = pd.DataFrame(data)
-    df['Amount_Dollars'] = df['Amount_Dollars']
-    df['Amount_BTC'] = df['Amount_BTC']
-    sort = df.sort_values('Amount_Dollars', ascending=False).head(1)
-
-    hashvalue = sort['Hashes'].values[0]
-    timestamp = sort['Time'].values[0]
-    amount_btc = str(sort['Amount_BTC'].values[0])
-    amount_dollars = str(sort['Amount_Dollars'].values[0])
-
-    highest = {"Hash": hashvalue , "Time": timestamp , "Amount_BTC" : amount_btc , "Amount_USD" : amount_dollars }
+    highest = {"Hash": topush[0], "Time": topush[1] , "Amount_BTC" : topush[2] , "Amount_USD" : topush[3] }
     x = col_transactions.insert_one(highest)
 
     xID = x.inserted_id
     print(col_transactions.find_one(xID))
 
-    
+    keylist.clear()
+    r.flushdb()
+
+
 
 #constants
 CLIENT = mongo.MongoClient ("mongodb://127.0.0.1:27017")    
 transactions_db = CLIENT["Transactions"]
 col_transactions = transactions_db["transactions"]
+keylist = []
 
 try:
     while True:
                 
         scraper()
-        time.sleep(60)
+        time.sleep(10)
+        scraper()
+        time.sleep(10)
+        scraper()
+        time.sleep(10)
+        scraper()
+        time.sleep(10)
+        scraper()
+        time.sleep(10)
+        scraper()
+        push()
+        time.sleep(10)
+        
 
 except KeyboardInterrupt:
     print(' interrupted!')
+
 
